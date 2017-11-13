b92e3127c6b90e0ddf37e0e22cdd285fd4a13a73
Amarjeet Singh Rai <git@arai.dev>
2017-11-13 12:25:53 +0000

    Create nodes and relationships in one command

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const neo4j = require('neo4j-driver').v1
 
 // const driver = neo4j.driver("bolt://52.91.126.14:33363", neo4j.auth.basic("neo4j", "answer-accruals-fractures"));
 const driver = neo4j.driver("bolt://localhost", neo4j.auth.basic("neo4j", ""))
-const stream = fs.createReadStream('user_result.csv')
+const stream = fs.createReadStream('rel_result.csv')
 
 const promises = []
 
@@ -14,7 +14,10 @@ csv
     let session = driver.session()
     promises.push(
       session
-        .run('MERGE (u:User {id: {idParam}})', {idParam: data.id})
+        .run(
+          'MERGE (u1:User { id: {follower} }) MERGE (u2:User { id: {followed} }) MERGE (u1)-[r:FOLLOWING]->(u2)',
+          { follower: data.followerUserId, followed: data.followedUserId }
+        )
         .then(function (result) {
           result.records.forEach(function (record) {
             console.log(record.get('u.id'));
