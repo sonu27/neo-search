2ae33e95d56d4b799de6f484af7b7cb05b15e9f1
Amarjeet Singh Rai <git@arai.dev>
2017-11-13 12:24:47 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const csv = require('fast-csv')
+const fs = require('fs')
+const neo4j = require('neo4j-driver').v1
+
+// const driver = neo4j.driver("bolt://52.91.126.14:33363", neo4j.auth.basic("neo4j", "answer-accruals-fractures"));
+const driver = neo4j.driver("bolt://localhost", neo4j.auth.basic("neo4j", ""))
+const stream = fs.createReadStream('user_result.csv')
+
+const promises = []
+
+csv
+  .fromStream(stream, {headers: true})
+  .on('data', (data) => {
+    let session = driver.session()
+    promises.push(
+      session
+        .run('MERGE (u:User {id: {idParam}})', {idParam: data.id})
+        .then(function (result) {
+          result.records.forEach(function (record) {
+            console.log(record.get('u.id'));
+          })
+          session.close();
+        })
+        .catch(function (error) {
+          console.log(error)
+        })
+    )
+  })
+  .on('end', () => {
+    console.log('CSV Done')
+    Promise.all(promises).then(() => {
+      driver.close()
+      console.log('Fully Done')
+    })
+  })
