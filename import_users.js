610de59d0aabc5184b48f998e083710b3beba61d
Amarjeet Singh Rai <git@arai.dev>
2017-11-14 16:43:41 +0000

    Separate queries onto multiple lines

--- import_users.js
+++ import_users.js
@@ -3,9 +3,11 @@ const driver = neo4j.driver("bolt://localhost", neo4j.auth.basic("neo4j", ""))
 let session = driver.session()
 
 session
-  .run(
-    'USING PERIODIC COMMIT LOAD CSV WITH HEADERS FROM "file:///users.csv" AS row CREATE (:User { id: row.id })'
-  )
+  .run(`
+    USING PERIODIC COMMIT
+    LOAD CSV WITH HEADERS FROM "file:///users.csv" AS row
+    CREATE (:User { id: row.id })
+  `)
   .then(function () {
     session.close()
     driver.close()
