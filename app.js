2c4e53ea9b8cb3753aca91c2f80d8f261b6657cb
Amarjeet Singh Rai <git@arai.dev>
2017-11-27 17:13:09 +0000

    Add related search (WIP)

--- app.js
+++ app.js
@@ -6,11 +6,33 @@ const neo4j = require('neo4j-driver').v1
 const driver = neo4j.driver(`bolt://${c.NEO4J_HOST}`, neo4j.auth.basic(c.NEO4J_USER, c.NEO4J_PASS))
 const Neo4jClient = require('./Neo4jClient')(driver)
 
+const elasticsearch = require('elasticsearch')
+const esConfig = {host: `${c.ES_HOST}:${c.ES_PORT}`}
+const EsClient = require('./ElasticsearchClient')(new elasticsearch.Client(esConfig))
+
 app.get('/professions/:id/related', function (req, res) {
   Neo4jClient.getRelatedProfessions(req.params.id)
     .then((data) => {
       res.send({professions: data})
     })
 })
+
+app.get('/users', async function (req, res) {
+  // Neo4jClient.getUsersByProfession(req.query.profession)
+  // .then((data) => {
+  //   res.send({users: data})
+  // })
+
+  // const data = await EsClient.search(req.query.profession)
+
+  const professionIds = await Neo4jClient.searchProfessions(req.query.profession)
+
+
+  const data = await EsClient.searchByProfessionIds(professionIds)
+
+  res.send({users: data})
+})
+
+
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
