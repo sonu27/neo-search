3619e03ca9e7a15e347fe78e021aee9e6cad9dc9
Amarjeet Singh Rai <git@arai.dev>
2017-11-30 15:47:02 +0000

    Enable cors

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const c = require('./config')
 const express = require('express')
 const app = express()
+const cors = require('cors')
 
 const neo4j = require('neo4j-driver').v1
 const driver = neo4j.driver(`bolt://${c.NEO4J_HOST}`, neo4j.auth.basic(c.NEO4J_USER, c.NEO4J_PASS))
@@ -9,6 +10,8 @@ const Neo4jClient = require('./Neo4jClient')(driver)
 const elasticsearch = require('elasticsearch')
 const esConfig = {host: `${c.ES_HOST}:${c.ES_PORT}`}
 const EsClient = require('./ElasticsearchClient')(new elasticsearch.Client(esConfig))
+
+app.use(cors())
 
 app.get('/professions/:id/related', function (req, res) {
   Neo4jClient.getRelatedProfessions(req.params.id)
