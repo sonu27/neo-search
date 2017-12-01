8c0636644c87fb98449ad8c1afded5aaa5f1526e
Amarjeet Singh Rai <jatt27@gmail.com>
2017-12-01 08:25:22 +0000

    Update tests

--- Neo4jClient.test.js
+++ Neo4jClient.test.js
@@ -1,10 +1,23 @@
 const c = require('./config')
 const neo4j = require('neo4j-driver').v1
-const driver = neo4j.driver(`bolt://${c.NEO4J_HOST}`, neo4j.auth.basic(c.NEO4J_USER, c.NEO4J_PASS))
-const Neo4jClient = require('./Neo4jClient')(driver)
 
-test('some', async () => {
-  const result = await Neo4jClient.searchProfessions('design')
-  console.log(await result)
+let driver
+let Neo4jClient
+
+beforeEach(() => {
+  driver = neo4j.driver(`bolt://${c.NEO4J_HOST}`, neo4j.auth.basic(c.NEO4J_USER, c.NEO4J_PASS))
+  Neo4jClient = require('./Neo4jClient')(driver)
+});
+
+afterEach(() => {
   driver.close()
+});
+
+test('search professions', async () => {
+  await expect(Neo4jClient.searchProfessions('design')).resolves.toBeDefined()
+})
+
+test('related professions', async () => {
+  //graphic designer: 70944
+  await expect(Neo4jClient.getRelatedProfessions(70944)).resolves.toBeDefined()
 })
