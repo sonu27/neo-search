962d7b4153a714e2ed1792ed510f2076d17cca88
Amarjeet Singh Rai <git@arai.dev>
2017-12-07 10:50:30 +0000

    Use bulk upload when sending to ES

--- 3-1-send-users-to-es-from-neo.js
+++ 3-1-send-users-to-es-from-neo.js
@@ -2,23 +2,56 @@ const c = require('./config')
 const fs = require('fs')
 const neo4j = require('neo4j-driver').v1
 const driver = neo4j.driver(`bolt://${c.NEO4J_HOST}`, neo4j.auth.basic(c.NEO4J_USER, c.NEO4J_PASS))
-const neoEs = require('./NeoEsSender')(driver)
+const Neo4jClient = require('./Neo4jClient')(driver)
+const elasticsearch = require('elasticsearch')
+const esClient = new elasticsearch.Client({host: `${c.ES_HOST}:${c.ES_PORT}`})
 
 const session = driver.session()
 let count = 0
+let total = 0
 let promise = Promise.resolve()
+let users = []
 
 session
   .run('MATCH (u:User) return u.id AS id')
   .subscribe({
     onNext: function (record) {
       let id = record.get('id').toNumber()
-      count++
+      total++
 
-      promise = promise.then(() => neoEs.send(id))
+      promise = promise.then(
+        () => new Promise((resolve, reject) => {
+          Neo4jClient.getUser(id).then(user => {
+            count++
+            users.push(user)
+            if (users.length === 1000 || count === total) {
+              let body = []
+              users.forEach(user => {
+                body.push({ index:  { _index: 'search', _type: 'user', _id: user.id } })
+                body.push(user)
+              })
+              users = []
+
+              esClient.bulk({
+                body: body
+              }, function (error, resp) {
+                console.log(error)
+              })
+              console.log(count)
+            }
+
+            if (count === total) {
+              driver.close()
+            }
+
+            resolve()
+          })
+        })
+      )
     },
     onCompleted: function () {
-      console.log('all users returned', count)
+      console.log('all users returned', total)
+
       session.close()
     },
     onError: function (error) {
