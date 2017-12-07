3551c2a37f083e88d997906d81cea485d3407b91
Amarjeet Singh Rai <git@arai.dev>
2017-12-07 10:50:30 +0000

    Use bulk upload when sending to ES

--- RecordTransformer.js
+++ RecordTransformer.js
@@ -9,19 +9,18 @@ module.exports = RecordTransformer = () => {
   return {
     'toObject': (record) => {
       let level = record.get('level')
-      let o = {
+      let user = {
         'id': record.get('id').toNumber(),
         'firstName': record.get('firstName'),
         'lastName': record.get('lastName'),
         'level': level == null ? 0 : level.toNumber(),
         'createdAt': record.get('createdAt'),
-        'usersFirstDegree': arrayToNumbers(record.get('usersFirstDegree')),
-        //'usersSecondDegree': arrayToNumbers(record.get('usersSecondDegree')),
+        'usersFollowing': arrayToNumbers(record.get('usersFollowing')),
         'professionIds': arrayToNumbers(record.get('professionIds')),
         'professions': record.get('professions'),
       }
 
-      return o
+      return user
     }
   }
 }
