e25f7812fdef86323e255952708b8a9a08035831
Amarjeet Singh Rai <git@arai.dev>
2018-01-05 17:07:26 +0000

    Remove unneeded folders

--- import_sql/professions.sql
+++
@@ -1,9 +0,0 @@
-select
-  distinct p.id as id,
-  p.title as name
-from professions p
-inner join user_professions up
-on p.id = up.professionId
-inner join users u
-on up.userId = u.id
-where u.deletedAt is null
