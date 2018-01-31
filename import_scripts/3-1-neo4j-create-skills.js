799e549b0215c51507aacd7abcaefa716684f32b
Amarjeet Singh Rai <jatt27@gmail.com>
2018-01-31 12:03:46 +0000

    Reduce files

--- import_scripts/3-1-neo4j-create-skills.js
+++ import_scripts/3-1-neo4j-create-skills.js
@@ -3,18 +3,23 @@ const neo4j = require('neo4j-driver').v1
 const driver = neo4j.driver(`bolt://${c.NEO4J_HOST}`, neo4j.auth.basic(c.NEO4J_USER, c.NEO4J_PASS))
 const session = driver.session()
 
+console.log('Neo4j creating skills')
+
 session
-  .run(`
-    USING PERIODIC COMMIT
-    LOAD CSV WITH HEADERS FROM "file:///skills.csv" AS row
-    CREATE (
-      :Skill {
-        id: toInteger(row.id),
-        name: row.name,
-        visible: toBoolean(row.visible)
-      }
-    )
-  `)
+  .run(`CREATE CONSTRAINT ON (s:Skill) ASSERT s.id IS UNIQUE`)
+  .then(() => {
+    return session.run(`
+      USING PERIODIC COMMIT
+      LOAD CSV WITH HEADERS FROM "file:///skills.csv" AS row
+      CREATE (
+        :Skill {
+          id: toInteger(row.id),
+          name: row.name,
+          visible: toBoolean(row.visible)
+        }
+      )
+    `)
+  })
   .then(function () {
     session.close()
     driver.close()
