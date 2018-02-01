05cf562c78f4903992160c81fa9ecc2a2e994eae
Amarjeet Singh Rai <git@arai.dev>
2018-02-01 15:56:35 +0000

    Super speed!

--- src/Neo4jClient.js
+++ src/Neo4jClient.js
@@ -202,20 +202,15 @@ module.exports = Neo4jClient = (driver) => {
         session
           .run(`
             MATCH (u:User {id: ${id}})
-            OPTIONAL MATCH (u)-[:HAS_PROFESSION]->(p)
-            OPTIONAL MATCH (u)-[:HAS_INDUSTRY]->(i)
-            OPTIONAL MATCH (u)-[:HAS_EXPERIENCE]->(e)
-            OPTIONAL MATCH (u)-[:HAS_SKILL]->(s)
-            OPTIONAL MATCH (u)-[:FOLLOWS]->(u1)
             RETURN
               u,
-              collect(DISTINCT u1.id) AS usersFollowing,
-              collect(DISTINCT p.id) AS professionIds,
-              collect(DISTINCT p.name) AS professions,
-              collect(DISTINCT s.id) AS skillIds,
-              collect(DISTINCT s.name) AS skills,
-              collect(DISTINCT i.name) AS industries,
-              collect(DISTINCT e.name) AS experiences
+              [(u)-[:FOLLOWS]->(u1) | u1.id] AS usersFollowing,
+              [(u)-[:HAS_PROFESSION]->(p) | p.id] AS professionIds,
+              [(u)-[:HAS_PROFESSION]->(p) | p.name] AS professions,
+              [(u)-[:HAS_SKILL]->(s) | s.id] AS skillsId,
+              [(u)-[:HAS_SKILL]->(s) | s.name] AS skills,
+              [(u)-[:HAS_INDUSTRY]->(i) | i.name] AS industries,
+              [(u)-[:HAS_EXPERIENCE]->(e) | e.name] AS experiences
           `)
           .subscribe({
             onNext: function (record) {
